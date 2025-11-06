325a61935a32b07a78b80d56ce89ff73c7f33ea4
Neil Geniebla <ndgeniebla2@gmail.com>
2025-11-06 15:11:57 -0800

    remove justify-center of top parent div

--- src/app/page.js
+++ src/app/page.js
@@ -27,8 +27,8 @@ const projects = [
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-4xl flex-col items-center justify-between py-8 px-4 bg-zinc-50 dark:bg-black sm:items-start">
+    <div className="flex flex-col min-h-screen items-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex w-full max-w-4xl flex-col items-center justify-between py-8 px-4 bg-zinc-50 dark:bg-black sm:items-start">
         {/* had to move sticky to the top parent because w-full on the nav bar itself refused to work. I LOVE SHADCN!!!! */}
         <div className="w-full sticky top-0 bg-zinc-50 font-sans dark:bg-black">
           <MyNavBar/>
